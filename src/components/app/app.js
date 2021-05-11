1f0896bad1213fa4a927a7f37a3b60e949097b5b
danil_lans <danimen0001@gmail.com>
2021-05-11 09:35:01 +0300

    update app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,1 +1,24 @@
+import React, { Component } from "react";
+import { Col, Row, Container, Button } from "../../../node_modules/reactstrap";
+import Header from "../header";
 import CharacterPage from "../characterPage/";
+export default class App extends Component {
+	state = {
+	};
+	render() {
+		return (
+			<>
+				<Container>
+					<Header />
+				</Container>
+				<Container>
+					<Row>
+					</Row>
+					<CharacterPage />
+					<CharacterPage />
+					<CharacterPage />
+				</Container>
+			</>
+		);
+	}
+}
