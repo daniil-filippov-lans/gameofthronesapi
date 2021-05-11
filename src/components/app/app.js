7dc4501492d0b42a07f42c375bb0c6e2c292b387
danil_lans <danimen0001@gmail.com>
2021-05-12 00:13:23 +0300

    add routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,15 @@ import { Col, Row, Container, Button } from "../../../node_modules/reactstrap";
 import Header from "../header";
 import RandomChar from "../randomChar";
 import ErrorMassage from "../errorMassege";
-import CharacterPage from "../characterPage/";
+import ItemList from "../itemList";
+import CharDetails from "../itemDetails";
+import gotService from "../../services/gotService";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { BooksPage, HousesPage, CharacterPage, BooksItem } from "../pages";
 
 export default class App extends Component {
+	gotService = new gotService();
+
 	state = {
 		showRandomChar: true,
 		error: false,
@@ -34,22 +39,41 @@ export default class App extends Component {
 			return <ErrorMassage />;
 		}
 		return (
-			<>
-				<Container>
-					<Header />
-				</Container>
-				<Container>
-					<Row>
-						<Col lg={{ size: 5, offset: 0 }}>{randomChar}</Col>
-					</Row>
-					<Button color="secondary" onClick={this.toggleRandomChar}>
-						Toggle random character
-					</Button>
-					<CharacterPage />
-					<CharacterPage />
-					<CharacterPage />
-				</Container>
-			</>
+			<Router>
+				<div className="app">
+					<Container>
+						<Header />
+					</Container>
+					<Container>
+						<Row>
+							<Col lg={{ size: 5, offset: 0 }}>
+								{randomChar}
+								<Button
+									color="secondary"
+									onClick={this.toggleRandomChar}
+								>
+									Toggle random character
+								</Button>
+							</Col>
+						</Row>
+						<Route
+							path="/"
+							exact
+							component={() => <h1>Welcome to GOT DB</h1>}
+						/>
+						<Route path="/characters" component={CharacterPage} />
+						<Route path="/houses" component={HousesPage} />
+						<Route path="/books" exact component={BooksPage} />
+						<Route
+							path="/books/:id"
+							render={({ match }) => {
+								const { id } = match.params;
+								return <BooksItem bookId={id} />;
+							}}
+						/>
+					</Container>
+				</div>
+			</Router>
 		);
 	}
 }
