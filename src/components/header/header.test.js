6a789b19abf52307a693efb850053bf002247c5c
danil_lans <danimen0001@gmail.com>
2021-08-02 18:28:53 +0300

    обновил снепшоты

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './header';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 describe('Testing <Header/>', () => {
     it('Header have rendered correctly', () => {
         const header = shallow(<Header />);
-        expect(header).toMatchSnapshot();
+        expect(toJSON(header)).toMatchSnapshot();
     });
 });
--- src/components/randomChar/randomChar.test.js
+++ src/components/randomChar/randomChar.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import RandomChar from './randomChar';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 describe('Testing <RandomChar/>', () => {
     it('RandomChar rendered correctly', () => {
         const char = shallow(<RandomChar />);
-        expect(char).toMatchSnapshot();
+        expect(toJSON(char)).toMatchSnapshot();
     });
+
+    // it('RandomChar state "char" is empty object', () => {
+    //     const char = shallow(<RandomChar />);
+    //     expect(char.state().char).toBeObject();
+    // });
 });
